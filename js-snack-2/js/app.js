e4fac20caa507eab71d126a1ab9bc10b779997ed
alemalagni <alemalagni@gmail.com>
2025-02-27 12:16:38 +0100

    feat: js snacks 2

--- js-snack-2/js/app.js
+++ js-snack-2/js/app.js
@@ -1,3 +1,13 @@
 const p1 = prompt();
 const p2 = prompt();
 
+if ( p1.length < p2.length ) {
+    console.log(p1)
+    console.log(p2)
+} else if ( p1.length > p2.length ) {
+    console.log(p2)
+    console.log(p1)
+} else if ( p1.length === p2.length ) {
+    console.log(p1)
+    console.log(p2)
+}
