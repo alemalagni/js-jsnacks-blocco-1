2dcdf418972d3295e5981013b5cb6268e2f867e2
alemalagni <alemalagni@gmail.com>
2025-02-27 12:29:27 +0100

    fix: JS snack 1

--- js-snack-1/js/app.js
+++ js-snack-1/js/app.js
@@ -1,12 +1,14 @@
 const n1 = prompt();
 const n2 = prompt();
 
-if (n1 > n2) {
-    console.log(n1)
-} else if ( n2 > n1 ) {
-    console.log(n2)
-} else if ( n1 === n2 ) {
-    console.log(n1)
+if ( isNaN(n1) === false && isNaN(n2) === false ){
+    if (n1 > n2) {
+        console.log(n1)
+    } else if ( n2 > n1 ) {
+        console.log(n2)
+    } else if ( n1 === n2 ) {
+        console.log(n1)
+    }
 } else {
     console.log("Inserire due numeri validi")
 }
